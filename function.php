02145190e576ff8a74b7349060095310b66f79bc
Samuel-08 <79854858+Samuel-08@users.noreply.github.com>
2021-08-26 18:14:05 +0800

    5
    penambahan function

---
+++ function.php
@@ -0,0 +1,15 @@
+<?php 
+
+get_required_files()
+mysqli_connect()
+mysqli_fetch()
+mysqli_fetch_array(result)
+mysqli_fetch_object(result)
+mysqli_fetch_row(result)
+mysqli_fetch_assoc(result)
+
+mysql_query(query)
+mysql_result(result, row)
+mysql_affected_rows()
+?>
+ech
